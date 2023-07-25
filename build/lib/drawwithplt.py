5032f0213116d42bb0d4096d99df1f4287afc6a2
liftes <17271113@bjtu.edu.cn>
2023-07-25 10:39:06 +0800

    0.3.1更新

--- build/lib/drawwithplt.py
+++ build/lib/drawwithplt.py
@@ -310,6 +310,102 @@ def polyfit(x_arr,y_arr,step=1000,order=3,delete=1,turn=False,c="gray",label="",
             ax.plot(x,y,color=c,label=label)
     return p1
 
+def Initprop_forDrawMaps(prop, data):
+    # 图片尺寸参数
+    prop.setdefault("xl", 2.5)
+    prop.setdefault("yl", 1.5)
+    prop.setdefault("aspect", "auto")
+
+    # 图片横纵间隔
+    prop.setdefault("space", (0.5, 0.5))
+    prop.setdefault("cbar_weight", 0.08)
+
+    # 绘图参数-cbar
+    prop.setdefault("c_labels", 'Cbar')
+    prop.setdefault("cbar_str", 'seismic')
+    prop.setdefault("vmin", np.min(data[(0,0)]))
+    prop.setdefault("vmax", np.max(data[(0,0)]))
+    prop.setdefault("vs", 6)
+    prop.setdefault("v_fmt", "%.2f")
+
+    # 刻度参数
+    prop.setdefault("x0_ticks", [])
+    prop.setdefault("x0_tlabels", [])
+    prop.setdefault("x0_labels", "x-%d")
+
+    prop.setdefault("x_ticks", [])
+    prop.setdefault("x_tlabels", [])
+    prop.setdefault("x_labels", "")
+    prop.setdefault("x_fmt", "%.2f")
+
+    prop.setdefault("y0_ticks", [])
+    prop.setdefault("y0_tlabels", [])
+    prop.setdefault("y0_labels", "y-%d")
+    
+    prop.setdefault("y_ticks", [])
+    prop.setdefault("y_tlabels", [])
+    prop.setdefault("y_labels", "")
+    prop.setdefault("y_fmt", "%.2f")
+
+    return prop
+
+
+
+def DrawMaps(xlist, ylist, data, prop = {}):
+    x = len(xlist)
+    y = len(ylist)
+    prop = Initprop_forDrawMaps(prop, data)
+
+    fig = plt.figure(figsize=(prop["yl"]*y, prop["xl"]*x))
+    fig.subplots_adjust(hspace=prop["space"][0], wspace=prop["space"][1])
+    width_ratios = [1] * y
+    width_ratios.append(prop["cbar_weight"])
+    gs = gridspec.GridSpec(x, y+1, width_ratios=width_ratios)
+    for xv, xi in enumerate(xlist):
+        for yv, yi in enumerate(ylist):
+            ax = plt.subplot(gs[xi,yi])
+            tmpdata = data[(xi,yi)]
+
+            im = ax.imshow(tmpdata, cmap=prop["cbar_str"], origin='lower', vmin=prop["vmin"], vmax=prop["vmax"], aspect=prop["aspect"])
+
+            if xi == x-1:
+                ax.set_xticks(prop["x0_ticks"])
+                ax.set_xticklabels([prop["x_fmt"]%i for i in prop["x0_tlabels"]])
+                if "%" in prop["x0_labels"]:
+                    ax.set_xlabel(prop["x0_labels"]%xv)
+                else:
+                    ax.set_ylabel(prop["x0_labels"])
+            else:
+                ax.set_xticks(prop["x_ticks"])
+                ax.set_xticklabels([prop["x_fmt"]%i for i in prop["x_tlabels"]])
+                if "%" in prop["x_labels"]:
+                    ax.set_xlabel(prop["x_labels"]%xv)
+                else:
+                    ax.set_ylabel(prop["x_labels"])
+
+            if yi == 0:
+                ax.set_yticks(prop["y0_ticks"])
+                ax.set_yticklabels([prop["y_fmt"]%i for i in prop["y0_tlabels"]])
+                if "%" in prop["y0_labels"]:
+                    ax.set_ylabel(prop["y0_labels"]%yv)
+                else:
+                    ax.set_ylabel(prop["y0_labels"])
+            else:
+                ax.set_yticks(prop["y_ticks"])
+                ax.set_yticklabels([prop["y_fmt"]%i for i in prop["y_tlabels"]])
+                if "%" in prop["y_labels"]:
+                    ax.set_ylabel(prop["y_labels"]%yv)
+                else:
+                    ax.set_ylabel(prop["y_labels"])
+
+    fig.align_ylabels()
+    ax = plt.subplot(gs[:,-1])
+    cbar = fig.colorbar(im, cax=ax)
+    cbar.set_ticks(np.linspace(prop["vmin"], prop["vmax"], prop["vs"]))
+    cbar.set_ticklabels([prop["v_fmt"]%i for i in np.linspace(prop["vmin"], prop["vmax"], prop["vs"])])
+    cbar.set_label(prop["c_labels"])
+
+
 
 # 自定义色卡区
 CSL_4_1 = ["#384259","#f73859","#7ac7c4","#f07b3f"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='drawwithplt',
-    version=0.2,
+    version="0.3.1",
     description=(
         'More EASY to Draw SCI figure with PIL'
     ),
